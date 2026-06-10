df151aaf213ca37c012e1dcf827daa5710a73692
Patrick Ranchi <ranchipatrick04@gmail.com>
2026-06-10 17:34:17 +0200

    EStatoInserito

---
+++ Entity/EStatoInserito.php
@@ -0,0 +1,8 @@
+<?php/**
+ * Stato: Opera inserita solo per esposizione (Social).
+ */
+class EStatoInserito extends EStatoOpera {
+    public function __construct() { $this->nomeStato = "Solo Esposizione"; }
+    public function isVendibile(): bool { return false; }
+    public function puoEssereModificata(): bool { return true; }
+}
